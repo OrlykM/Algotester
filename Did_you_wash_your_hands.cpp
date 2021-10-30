a11056e1f49ca80510066cf63a8ead3ca823bf4e
Maxim <87619450+OrlykM@users.noreply.github.com>
2021-10-30 14:12:54 +0300

    Add files via upload

---
+++ Did_you_wash_your_hands.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, k;
+    int n = 0;
+    cin >> a >> b >> k;
+    for (int i = 0; i < k; i++)
+    {
+        if (i == 0)
+        {
+            n += a + b;
+        }
+        else
+        {
+            n += b;
+        }
+    }
+    cout << n;
+
+    return 0;
+}
